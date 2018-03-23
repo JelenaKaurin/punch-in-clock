af3779abbedeb54b484a92ae34d06f266848a07d
JelenaKaurin <jelenakaurin@live.com>
2018-03-23 12:07:07 +0100

    2 pokusaj

--- src/urls.py
+++ src/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
+from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('core/', include('core.urls')),
+
 ]
 
 if settings.DEBUG:
 
     urlpatterns += staticfiles_urlpatterns()
+
